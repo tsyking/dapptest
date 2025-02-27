1b8717e0e82435f9d3b1f8b1b14f94d5d25e9012
tsyking <47906754+tsyking@users.noreply.github.com>
2025-02-27 16:11:47 +0800

    Delete src/components directory

--- src/components/checkBalance.js
+++
@@ -1,7 +0,0 @@
-import Web3 from 'web3';
-
-export default async function checkBalance(address) {
-    const web3 = new Web3(window.ethereum);
-    const balance = await web3.eth.getBalance(address);
-    return web3.utils.fromWei(balance, 'ether');
-}
